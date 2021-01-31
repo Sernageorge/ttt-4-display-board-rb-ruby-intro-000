f4bd4685250da7466a755bf9bb24b77b381af4ef
George Serna <sernageorge@gmail.com>
2021-01-31 20:51:05 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,6 +8,6 @@ def display_board(board)
   puts [" #{board[6]} | #{board[7]} | #{board[8]} "]
 end
 
-board = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
+board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
 
 display_board(board)
